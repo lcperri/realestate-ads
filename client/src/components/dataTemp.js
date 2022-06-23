013ad7c679e0ac1f598ea5082b3d40d972a2539d
lcperri <prluisca@gmail.com>
2022-06-23 13:44:42 -0500

    arraydata temp adicional

---
+++ client/src/components/dataTemp.js
@@ -0,0 +1,70 @@
+const propertiesList = [
+    {
+      id: 1,
+      address: 'Av. La perla 534 Buenos aires',
+      area: 84,
+      type: 'Casa',
+      rooms: 2,
+      status: 'Disponible',
+      city: 'Buenos aires',
+      bathrooms: 2,
+      neighbourhood: 2,
+      constructionDate: '01-31-1990',
+      parkingSlot: false,
+      rentPrice: 200,
+      sellPrice: 0,
+      pictures: 'https://images1.forrent.com/i2/KpZbXTacfBro7B9aSYndt_p4u_ZgQMLdjZWKRXeOGiU/117/image.jpg'
+    },
+  
+    {
+      id: 2,
+      address: 'Av. La perla 500 Rio de la plata',
+      area: 92,
+      type: 'Casa',
+      rooms: 2,
+      status: 'Disponible',
+      city: 'Buenos aires',
+      bathrooms: 2,
+      neighbourhood: 2,
+      constructionDate: '01-31-1990',
+      parkingSlot: false,
+      rentPrice: 300,
+      sellPrice: 0,
+      pictures: 'https://images1.forrent.com/i2/lSw0qLyu-bjaLi-ifsQYPCQ5eRHR2Q1TKDnN9bB62PM/117/image.jpg'
+    },
+  
+    {
+      id: 3,
+      address: 'Av. La perla 500 lumbreras',
+      area: 170,
+      type: 'Condominio',
+      rooms: 2,
+      status: 'Disponible',
+      city: 'Buenos aires',
+      bathrooms: 2,
+      neighbourhood: 2,
+      constructionDate: '01-31-1990',
+      parkingSlot: false,
+      rentPrice: 0,
+      sellPrice: 1900000,
+      pictures: 'https://gestion.pe/resizer/TkV3ialO6t0dLcT8ciedCa6X7B0=/580x330/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/44IPJVFIBJHUFCS4WG6XSTGPKQ.jpg'
+    },
+    {
+      id: 4,
+      address: 'Calle General Castilla 2344 La Paz',
+      area: 220,
+      type: 'Casa de playa',
+      rooms: 2,
+      status: 'Disponible',
+      city: 'Buenos aires',
+      bathrooms: 2,
+      neighbourhood: 2,
+      constructionDate: '01-31-1990',
+      parkingSlot: false,
+      rentPrice: 0,
+      sellPrice: 1900000,
+      pictures: 'https://img10.naventcdn.com/avisos/111/00/12/95/56/13/360x266/201715697.jpg'
+    }
+  ]
+
+  export default propertiesList
