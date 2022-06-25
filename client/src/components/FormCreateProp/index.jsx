55d2a8df31724ec63ec53371bbd0af01664a928b
fmarcos38 <fmarcos_23@hotmail.com>
2022-06-25 13:59:29 -0300

    comprobado q funciona la creacion-restan peqños detalles

--- client/src/components/FormCreateProp/index.jsx
+++ client/src/components/FormCreateProp/index.jsx
@@ -11,11 +11,11 @@ function validators(input){
 
     if(!input.neighbourhood){errors.neighbourhood = "El barrio es requerido!!"}
     if(!input.city){errors.city = "La ciudad es requerida!!"}
-    if(!input.adress){errors.adress = "La dirección es requerida!!"}
+    if(!input.address){errors.address = "La dirección es requerida!!"}
     if(!input.area) /* {errors.area = "El area es requerido!!"} */
     if(!input.rooms)/* {errors.rooms = "La cant de habitaciones es requerida!!"} */
     if(!input.bathrooms)/* {errors.bathrooms = "La cant de baños es requerida!!"} */
-    if(!input.pictures.length)/* {errors.pictures = "La carga de imagenes es requerida!!"} */
+    //if(!input.pictures.length)/* {errors.pictures = "La carga de imagenes es requerida!!"} */
 
     if(input.area < 0){errors.area = "No se permiten Numeros Negativos";} 
     if(input.rooms < 0){errors.rooms = "No se permiten Numeros Negativos";} 
@@ -28,20 +28,14 @@ function validators(input){
 
 export default function FormCreateProp(){
 
-    let initialState = {adress: "", rentPrice: "", sellPrice: "", area: "", type: "", rooms:"", 
-                        bathrooms: "", city: "", neighbourhood: "", constructionDate: "", renovationDate: "",
-                        parkingSlot: "", pictures: []};
-    const [input,setInput] = useState(initialState);
-    const [error, setError] = useState(initialState);
+    const [input,setInput] = useState({});
+    const [error, setError] = useState({});
     const dispatch = useDispatch();
 
-    //const allProps = useSelector(state => state.properties);//me traigo todas las props
-    //const prop = useSelector(state => state.property);//me traigo la prop creada
-    //let buscoProp = allProps.find(p => p.adress === input.adress);
 
-    /* useEffect(()=>{
+    useEffect(()=>{
         dispatch(getAllProperties())
-    }, [dispatch]); */
+    }, [dispatch]);
 
     const handleCH = (e) => {
         e.preventDefault();
@@ -59,21 +53,12 @@ export default function FormCreateProp(){
     const handlerS = (e) => {
         e.preventDefault();
 
-        /* if(!input.sellPrice && !input.rentPrice){alert("Debe ingresar el precio correspondiente a la operacion -> Venta/Alquiler")}
-        if(!input.adress || !input.area || !input.type || !input.rooms || !input.bathrooms || input.parkingSlot || !input.city ){
-            alert("Complete todos los campos!!");
-        }
-        if(( input.rooms || input.bathrooms ) < 0){
-            alert("No se permiten Numeros Negativos...!!"); 
-        }
-        else{  */
             dispatch(createProperty(input));            
             alert("Propiedad creada");
-            setInput({adress: "", rentPrice: "", sellPrice: "", area: "", type: "", rooms:"", 
-                    bathrooms: "", city: "", neighbourhood: "", constructionDate: "", renovationDate: "",
-                    parkingSlot: "", pictures: []});
-            //dispatch(getAllProperties());
-        //}
+            setInput({});
+            dispatch(getAllProperties());
+            setInput({});
+        
     };
 
     return(
@@ -85,7 +70,7 @@ export default function FormCreateProp(){
                 <select onChange={handleCH} id={"type"} className={"select-tipoProps"}>
                       <option>Tipo de propiedad: </option>
                       <option>Casa</option>
-                      <option>Departamento</option>
+                      <option>departamento</option>
                       <option>Ph</option>
                       <option>Local Comercial</option>
                       <option>Casa de campo</option>
@@ -100,20 +85,20 @@ export default function FormCreateProp(){
                       <option>Terreno agricola</option>
                 </select>
              
-                <input className={error.city ? "errorInput" : "input-ciudad"} type={'text'} id='city' value={input.city} onChange={handleCH} placeholder={'Ciudad'}/>
+                <input className={error.city ? "errorInput" : "input-ciudad"} type={'text'} id={'city'} value={input.city} onChange={handleCH}/>
                   
                 <input className={error.neighbourhood ? "errorInput" : "input-barrio"} type={'text'} id={'neighbourhood'} value={input.neighbourhood} onChange={handleCH} placeholder={"Barrio/Vecindario"}/>
                 <br></br>
                   
                 <label className="label-direcc">Direccion: </label>
-                <input className={error.adress ? "error-direcc" : "input-direcc"} type={'text'} id={'adress'} value={input.adress} onChange={handleCH}/>
+                <input className={error.address ? "error-direcc" : "input-direcc"} type={'text'} id={'address'} value={input.address} onChange={handleCH}/>
                  {/* {buscoProp && (<span className="span-error">Ya existe una Prop para dicha dirección!!</span>)} */}
                 <div className="div-precios">
                     <label className="label-venta">Precio Venta: </label>
                     <input className="input-sellPrice" type={'number'} id={'sellPrice'} value={input.sellPrice} onChange={handleCH} min='500' max='1111111111'/> 
   
                     <label className="label-alquiler">Precio Alquiler: </label>
-                    <input className="input-rentPrice" type={'number'} id={'rentPrice'} value={input.rentPrice} onChange={handleCH} min='500' max='1111111111'/>
+                    <input className="input-rentPrice" type={'number'} id={'rentPrice'} value={input.rentPrice} onChange={handleCH} min='0' max='1111111111'/>
                 </div>
   
                 <label className="label-area">Area: </label>
@@ -137,10 +122,10 @@ export default function FormCreateProp(){
 
                 <div className="fechas">
                     <label className="label-fechConstruc">ConstructionDate: </label>
-                    <input className="fecha-construc" type={'date'} id={'constructionDate'} value={input.constructionDate} onChange={handleCH} />
+                    <input className="fecha-construc" type={'number'} id={'constructionDate'} value={input.constructionDate} onChange={handleCH} min={'1900'} max={'2022'}/>
   
                     <label className="label-fechRenov">RenovationDate: </label>
-                    <input className="fecha-construc" type={'date'} id={'renovationDate'} value={input.renovationDate} onChange={handleCH} />                            
+                    <input className="fecha-construc" type={'number'} id={'renovationDate'} value={input.renovationDate} onChange={handleCH} min={'1900'} max={'2022'}/>                            
                 </div>
   
                 <label className="label-cargaImagen">Cargar Imagen: </label>
