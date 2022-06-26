4d904c7252ecfae324f22a566a1d52016342b9f2
fmarcos38 <fmarcos_23@hotmail.com>
2022-06-26 15:41:44 -0300

    falta una actualizacion

--- client/src/components/FormCreateProp/index.jsx
+++ client/src/components/FormCreateProp/index.jsx
@@ -3,8 +3,6 @@ import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import './FormCreate.css';
 import {createProperty, getAllProperties} from '../../redux/actions';
-import DivContainer from "../../styledComponents/DivContainer";
-import Button from "../../styledComponents/Button";
 
 
 //--funcion para hacer validaciones----------------------------
@@ -23,32 +21,15 @@ function validators(input){
     if(input.rooms < 0){errors.rooms = "No se permiten Numeros Negativos";} 
     if(input.bathrooms < 0){errors.bathrooms = "No se permiten Numeros Negativos";}  
     if(input.parkingSlot < 0){errors.parkingSlot = "No se permiten Numeros Negativos";}
-    //Validación para que se active o desactive el botón de Submit form:
-    if (JSON.stringify(errors) === '{}') return {none: true}
+    
     return errors;
 }
 //--------------------------------------------------------------------------------------------
 
 export default function FormCreateProp(){
-    
-<<<<<<< HEAD
-    let initialState = {type: "", city: "", address:"", rentPrice:"", sellPrice: "", area:"", 
-            rooms: "", bathrooms: "", parkingSlot:false};
-=======
-    let initialState = {
-        type: "", 
-        city: "", 
-        address:"", 
-        price:0, 
-        area:"", 
-        rooms: 1, 
-        bathrooms:1,
-        parkingSlot:false
-    };
 
->>>>>>> 154abad33fcc023d7db463c348405bbff3cb99ec
-    const [input,setInput] = useState(initialState);
-    const [error, setError] = useState(initialState);
+    const [input,setInput] = useState({});
+    const [error, setError] = useState({});
     const dispatch = useDispatch();
 
 
@@ -58,12 +39,12 @@ export default function FormCreateProp(){
 
     const handleCH = (e) => {
         e.preventDefault();
-        if (e.target.id === 'type'){
+        if(e.target.id === 'type'){
             setInput({...input, type: e.target.value})
-        } else if(e.target.id === 'parkingSlot'){
+        }else if(e.target.id === 'parkingSlot'){
             if(e.target.value === 'Si'){setInput({...input, parkingSlot: true})}
             if(e.target.value === 'No'){setInput({...input, parkingSlot: false})}
-        } else{
+        }else{
             setInput({...input, [e.target.id]: e.target.value});
             setError(validators({...input, [e.target.id]: e.target.value}));
         }
@@ -71,27 +52,17 @@ export default function FormCreateProp(){
         
     const handlerS = (e) => {
         e.preventDefault();
-<<<<<<< HEAD
-        
-        if(!input.address || !input.sellPrice|| !input.area || !input.type || !input.rooms || !input.bathrooms || !input.city){
-=======
-        if(!input.price && !input.rentPrice)
-        if(!input.address || !input.area || !input.type || !input.rooms || !input.bathrooms || !input.city){
-        }
-        if(( input.rooms || input.bathrooms ) < 0){
->>>>>>> 154abad33fcc023d7db463c348405bbff3cb99ec
-        }
-        else{ 
+
             dispatch(createProperty(input));            
+            alert("Propiedad creada");
             setInput({});
-            // setInput({type: "", city: "", address:"", rentPrice:"", price:"", area:"", 
-            // bathrooms:"", neighbourhood:"",  constructionDate:"", parkingSlot:""});
-        }
+            dispatch(getAllProperties());
+            setInput({});
+        
     };
 
     return(
-        <DivContainer>
-        {/* <div className="cont-gral-create"> */}
+        <div className="cont-gral-create">
 
             <div className="div-rec">
                <form onSubmit={handlerS} className="cont-formulario">
@@ -159,7 +130,6 @@ export default function FormCreateProp(){
               </form>
             </div>
             
-        {/* </div> */}
-        </DivContainer>
+        </div>
     )
 }
