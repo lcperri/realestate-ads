40f34731a209afc5533542e2d89af46a2c0c99ef
lcperri <prluisca@gmail.com>
2022-06-26 20:06:11 -0500

    navbar estilos

--- client/src/components/FormCreateProp/index.jsx
+++ client/src/components/FormCreateProp/index.jsx
@@ -52,6 +52,7 @@ export default function FormCreateProp() {
     }, [dispatch]);
 
     const handleCH = (e) => {
+
         e.preventDefault();
         if (e.target.id === 'type') {
             setInput({ ...input, type: e.target.value })
@@ -81,88 +82,6 @@ export default function FormCreateProp() {
 
     return (
         <DivContainer>
-<<<<<<< HEAD
-            {/* <div className="cont-gral-create"> */}
-            {/* <div className="div-recuadro"> */}
-                <form onSubmit={handlerS} >
-                    <select onChange={handleCH} id={"type"} className={"select-tipoProps"}>
-                        <option>Tipo de propiedad: </option>
-                        <option>Casa</option>
-                        <option>Departamento</option>
-                        <option>Ph</option>
-                        <option>Local Comercial</option>
-                        <option>Casa de campo</option>
-                        <option>Casa de playa</option>
-                        <option>Cochera</option>
-                        <option>Habitación</option>
-                        <option>Hotel</option>
-                        <option>Local Industrial</option>
-                        <option>Oficina</option>
-                        <option>Otros</option>
-                        <option>Terreno/Lote</option>
-                        <option>Terreno agricola</option>
-                    </select>
-
-                    <input className={error.city ? "errorInput" : "input-ciudad"} type={'text'} id={'city'} value={input.city} onChange={handleCH} />
-
-                    <input className={error.neighbourhood ? "errorInput" : "input-barrio"} type={'text'} id={'neighbourhood'} value={input.neighbourhood} onChange={handleCH} placeholder={"Barrio/Vecindario"} />
-                    <br></br>
-
-                    <label className="label-direcc">Direccion: </label>
-                    <input className={error.address ? "error-direcc" : "input-direcc"} type={'text'} id={'address'} value={input.address} onChange={handleCH} />
-
-                    {/* {buscoProp && (<span className="span-error">Ya existe una Prop para dicha dirección!!</span>)} */}
-                    <div className="div-precios">
-                        <select onChange={handleCH} id={"operation"} className={"select-tipoProps"}>
-                            <option>Quiero:</option>
-                            <option>Vender mi propiedad</option>
-                            <option>Rentar mi propiedad</option>
-                        </select>
-                        <div>
-                            Precio:
-                        </div>
-                        <div>
-                            <input className="input-price" type={'number'} id={'price'} value={input.price} onChange={handleCH} min='10' max='1111111111' />
-                        </div>
-                    </div>
-
-                    <label className="label-area">Area: </label>
-                    <input className={error.area ? "error-caract" : "input-area"} type={'text'} value={input.area} id={'area'} onChange={handleCH} />
-                    {error.area && (<div><span className='span-error-num'>{error.area}</span></div>)}
-
-                    <label className="label-area">Habitaciones: </label>
-                    <input className={error.area ? "error-caract" : "input-area"} type={'number'} min={'1'} max={'100'} value={input.rooms} id={'rooms'} onChange={handleCH} />
-                    {error.rooms && (<div><span className='span-error-num'>{error.rooms}</span></div>)}
-
-                    <label className="label-area">Baños: </label>
-                    <input className={error.area ? "error-caract" : "input-area"} type={'number'} min={'1'} max={'50'} value={input.bathrooms} id={'bathrooms'} onChange={handleCH} />
-                    {error.bathrooms && (<div><span className='span-error-num'>{error.bathrooms}</span></div>)}
-
-                    <label className="label-area">Cochera: </label>
-                    <select onChange={handleCH} id={'parkingSlot'} className={"cochera"}>
-                        <option>Si</option>
-                        <option>No</option>
-                    </select>
-                    {error.parkingSlot && (<div><span className='span-error-num'>{error.parkingSlot}</span></div>)}
-
-                    <div className="fechas">
-                        <div>
-                            <label >Año de construcción: </label>
-                            <input className="fecha-construc" type={'number'} id={'constructionDate'} value={input.constructionDate} onChange={handleCH} min={'1900'} max={'2022'} />
-                        </div>
-                        <div>
-                            <label >Año de renovación: </label>
-                            <input className="fecha-construc" type={'number'} id={'renovationDate'} value={input.renovationDate} onChange={handleCH} min={'1900'} max={'2022'} />
-                        </div>
-                    </div>
-
-                    <label className="label-cargaImagen">Cargar Imagen: </label>
-                    {/* <input className="cargaFotos" type={'file'}/> */}
-                    <input className="cargaFotos" type={'text'} id={'pictures'} value={input.pictures} onChange={handleCH} />
-
-                    <div>
-                        {/* <Button type={'submit'} disabled={!error.none} className='disabled'>
-=======
         {/* <div className="cont-gral-create"> */}
 
             <div className="div-recuadro">
@@ -235,16 +154,12 @@ export default function FormCreateProp() {
                 <input className="cargaFotos" type={'text'} id={'pictures'} value={input.pictures} onChange={handleCH}/>
   
                 <div>
-                    <Button type={'submit'} disabled={!error.none} className='disabled'>
->>>>>>> a38fdd25816e89f13d078818fc07e8f4f09f9f57
-                        Crear propiedad
-                    </Button> */}
                         <input className="boton-sub" type={'submit'} value={"Publicar Propiedad"} />
                     </div>
                 </form>
             {/* </div> */}
 
-            {/* </div> */}
+            </div>
         </DivContainer>
     )
 }
