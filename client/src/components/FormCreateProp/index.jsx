99a0e9bba4832cca7a98cf3c62ab0c42c9b7343d
fmarcos38 <fmarcos_23@hotmail.com>
2022-06-27 01:27:06 -0300

    formCreate props funcionando

--- client/src/components/FormCreateProp/index.jsx
+++ client/src/components/FormCreateProp/index.jsx
@@ -14,17 +14,18 @@ function validators(input) {
     if (!input.neighbourhood) { errors.neighbourhood = "El barrio es requerido!!" }
     if (!input.city) { errors.city = "La ciudad es requerida!!" }
     if (!input.address) { errors.address = "La dirección es requerida!!" }
-    if (!input.area) /* {errors.area = "El area es requerido!!"} */
-        if (!input.rooms)/* {errors.rooms = "La cant de habitaciones es requerida!!"} */
-            if (!input.bathrooms)/* {errors.bathrooms = "La cant de baños es requerida!!"} */
-                //if(!input.pictures.length)/* {errors.pictures = "La carga de imagenes es requerida!!"} */
+    if (!input.area)  {errors.area = "El area es requerido!!"} 
+    if (!input.rooms)/* {errors.rooms = "La cant de habitaciones es requerida!!"} */
+    if (!input.bathrooms)/* {errors.bathrooms = "La cant de baños es requerida!!"} */
+     //if(!input.pictures.length)/* {errors.pictures = "La carga de imagenes es requerida!!"} */
 
-                if (input.area < 0) { errors.area = "No se permiten Numeros Negativos"; }
+    if (input.area < 0) { errors.area = "No se permiten Numeros Negativos"; }
     if (input.rooms < 0) { errors.rooms = "No se permiten Numeros Negativos"; }
     if (input.bathrooms < 0) { errors.bathrooms = "No se permiten Numeros Negativos"; }
     if (input.parkingSlot < 0) { errors.parkingSlot = "No se permiten Numeros Negativos"; }
     //Validación para que se active o desactive el botón de Submit form:
     if (JSON.stringify(errors) === '{}') return { none: true }
+
     return errors;
 }
 //--------------------------------------------------------------------------------------------
@@ -35,11 +36,16 @@ export default function FormCreateProp() {
         type: "",
         city: "",
         address: "",
+        operation: "",
         price: 0,
         area: "",
         rooms: 1,
         bathrooms: 1,
-        parkingSlot: false
+        constructionDate: 1900,
+        renovationDate: 0,
+        neighbourhood: "",
+        parkingSlot: false,
+        pictures: []
     };
 
     const [input, setInput] = useState(initialState);
@@ -54,12 +60,18 @@ export default function FormCreateProp() {
     const handleCH = (e) => {
 
         e.preventDefault();
+
         if (e.target.id === 'type') {
             setInput({ ...input, type: e.target.value })
         } else if (e.target.id === 'parkingSlot') {
             if (e.target.value === 'Si') { setInput({ ...input, parkingSlot: true }) }
             if (e.target.value === 'No') { setInput({ ...input, parkingSlot: false }) }
-        } else {
+        }else if(e.target.id === 'operation'){
+            setInput({...input, operation: e.target.value});
+            if (e.target.value === 'Vender mi propiedad') { setInput({ ...input, operation: 'sell' }) }
+            if (e.target.value === 'Rentar mi propiedad') { setInput({ ...input, operation: 'rent' }) }
+        } 
+        else {
             setInput({ ...input, [e.target.id]: e.target.value });
             setError(validators({ ...input, [e.target.id]: e.target.value }));
         }
@@ -67,16 +79,28 @@ export default function FormCreateProp() {
 
     const handlerS = (e) => {
         e.preventDefault();
-        if (!input.price && !input.rentPrice)
-            if (!input.address || !input.area || !input.type || !input.rooms || !input.bathrooms || !input.city) {
-            }
-        if ((input.rooms || input.bathrooms) < 0) {
-        }
-        else {
+        
+        if (!input.address || !input.price || !input.area || !input.type || !input.rooms || !input.bathrooms || !input.city) {
+            alert("Faltan datos");
+        }else {
             dispatch(createProperty(input));
-            setInput({});
-            // setInput({type: "", city: "", address:"", rentPrice:"", price:"", area:"", 
-            // bathrooms:"", neighbourhood:"",  constructionDate:"", parkingSlot:""});
+            alert("Creado")
+            setInput({
+                type: "",
+                city: "",
+                address: "",
+                operation: "",
+                price: 0,
+                area: "",
+                rooms: 1,
+                bathrooms: 1,
+                constructionDate: 1900,
+                renovationDate: 0,
+                neighbourhood: "",
+                parkingSlot: false,
+                pictures: []
+            });
+            
         }
     };
 
@@ -84,10 +108,11 @@ export default function FormCreateProp() {
         <DivContainer>
         {/* <div className="cont-gral-create"> */}
 
-            <div className="div-recuadro">
-            <form onSubmit={handlerS} >
+            <div>
+                <form onSubmit={handlerS} >
                   
-                <select onChange={handleCH} id={"type"} className={"select-tipoProps"}>
+                    <label className="lab-tipoProp">Top de propiedad:</label> 
+                    <select onChange={handleCH} id={"type"} className={"select-tipoProps"}>
                       <option>Tipo de propiedad: </option>
                       <option>Casa</option>
                       <option>Departamento</option>
@@ -103,59 +128,58 @@ export default function FormCreateProp() {
                       <option>Otros</option>
                       <option>Terreno/Lote</option>
                       <option>Terreno agricola</option>
-                </select>
-             
-                <input className={error.city ? "errorInput" : "input-ciudad"} type={'text'} id={'city'} value={input.city} onChange={handleCH}/>
-                  
-                <input className={error.neighbourhood ? "errorInput" : "input-barrio"} type={'text'} id={'neighbourhood'} value={input.neighbourhood} onChange={handleCH} placeholder={"Barrio/Vecindario"}/>
-                <br></br>
-                  
-                <label className="label-direcc">Direccion: </label>
-                <input className={error.address ? "error-direcc" : "input-direcc"} type={'text'} id={'address'} value={input.address} onChange={handleCH}/>
-
-                 {/* {buscoProp && (<span className="span-error">Ya existe una Prop para dicha dirección!!</span>)} */}
-                
-                <select onChange={handleCH} id={"operation"} className={"select-tipoProps"}>
+                    </select>
+                                  
+                    <label className="lab-ciuda">Ciudad:</label> 
+                    <input className={error.city ? "errorInput" : "input-ciudad"} type={'text'} id={'city'} value={input.city} onChange={handleCH}/>
+               
+                    <label className="lab-barrio">Barrio/Vecindario:</label>
+                    <input className={error.neighbourhood ? "errorInput" : "input-barrio"} type={'text'} id={'neighbourhood'} value={input.neighbourhood} onChange={handleCH} placeholder={"Barrio/Vecindario"}/>
+                    <br></br>
+                    
+                    <label className="lab-direcc">Dirección:</label>
+                    <input className={error.address ? "error-direcc" : "input-direcc"} type={'text'} id={'address'} value={input.address} onChange={handleCH}/>
+                               
+                    <label className="lab-tipoOp">Tipo de Operación:</label>
+                    <select onChange={handleCH} id={"operation"} className={"select-operation"}>
                         <option>Quiero:</option>
                         <option>Vender mi propiedad</option>
                         <option>Rentar mi propiedad</option>
-                </select>
+                    </select>
+                    <br></br>
 
-                <label>Precio:</label>
-                <input className="input-price" type={'number'} id={'price'} value={input.price} onChange={handleCH} min='10' max='1111111111'/> 
-                    
-                <label className="label-area">Area: </label>
-                <input className={error.area ? "error-caract" : "input-area"} type={'text'} value={input.area} id={'area'} onChange={handleCH}/>
+                    <label className="lab-precio">Precio:</label>
+                    <input className="input-price" type={'number'} id={'price'} value={input.price} onChange={handleCH} min='10' max='1111111111' placeholder="Pecio Venta/Alquiler del Inmueble"/> 
+                  
+                    <label className="label-area">Area: </label>
+                    <input className={error.area ? "error-caract" : "input-area"} type={'text'} value={input.area} id={'area'} onChange={handleCH}/>
+                 
+                    <label className="label-area">Habitaciones: </label>
+                    <input className={error.area ? "error-caract" : "input-area"} type={'number'} min={'1'} max={'100'} value={input.rooms} id={'rooms'} onChange={handleCH}/>
                 
-                <label className="label-area">Habitaciones: </label>
-                <input className={error.area ? "error-caract" : "input-area"} type={'number'} min={'1'} max={'100'} value={input.rooms} id={'rooms'} onChange={handleCH}/>
-                
-                <label className="label-area">Baños: </label>
-                <input className={error.area ? "error-caract" : "input-area"} type={'number'} min={'1'} max={'50'} value={input.bathrooms} id={'bathrooms'} onChange={handleCH}/>
+                    <label className="label-area">Baños: </label>
+                    <input className={error.area ? "error-caract" : "input-area"} type={'number'} min={'1'} max={'50'} value={input.bathrooms} id={'bathrooms'} onChange={handleCH}/>
                 
 
-                <label className="label-area">Cochera: </label>
-                <select onChange={handleCH} id={'parkingSlot'} className={"cochera"}>
+                    <label className="label-area">Cochera: </label>
+                    <select onChange={handleCH} id={'parkingSlot'} className={"cochera"}>
                     <option>Si</option>
                     <option>No</option>
-                </select> 
-                {error.parkingSlot && (<div><span className='span-error-num'>{error.parkingSlot}</span></div>)}
+                    </select> 
+                    <br></br>
 
-                <div className="fechas">
-                    <label className="label-fechConstruc">ConstructionDate: </label>
-                    <input className="fecha-construc" type={'number'} id={'constructionDate'} value={input.constructionDate} onChange={handleCH} min={'1900'} max={'2022'}/>
+                    <label className="label-fechConstruc">Fecha Construcción: </label>
+                    <input className="input-fechaConstruc" type={'number'} id={'constructionDate'} value={input.constructionDate} onChange={handleCH} min={'1900'} max={'2022'}/>
   
-                    <label className="label-fechRenov">RenovationDate: </label>
-                    <input className="fecha-construc" type={'number'} id={'renovationDate'} value={input.renovationDate} onChange={handleCH} min={'1900'} max={'2022'}/>                            
-                </div>
+                    <label className="label-fechRenov">Fecha Renovación: </label>
+                    <input className="input-fechaRenov" type={'number'} id={'renovationDate'} value={input.renovationDate} onChange={handleCH} min={'1900'} max={'2022'}/>                            
+                
+                    <br></br>
+                    <label className="cargImagen">Subir Imagen:</label>
+                    <input className="input-cargImagen" type={'text'} id={'pictures'} value={input.pictures} onChange={handleCH}/>
   
-                <label className="label-cargaImagen">Cargar Imagen: </label>
-                {/* <input className="cargaFotos" type={'file'}/> */}
-                <input className="cargaFotos" type={'text'} id={'pictures'} value={input.pictures} onChange={handleCH}/>
-  
-                <div>                    
-                  <input className="boton-sub" type={'submit'} value={"Subir Propiedad"}/>
-                </div>
+                    <br></br>
+                    <input className="boton-sub" type={'submit'} value={"Subir Propiedad"}/>               
                 
               </form>
             </div>
