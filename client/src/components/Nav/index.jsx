2f9f9c35b6b1fac9d9314eb2f2a5cfbec1a6f223
Julian <julianmeraviglia@gmail.com>
2022-07-10 18:10:28 -0300

    Landing mejorada, con actualizacion de todo el proyecto

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -53,6 +53,7 @@ const Nav = () => {
             <>
               <StyledHash to={'#'}>Inicio</StyledHash>
               <StyledHash to="#how-it-works">Cómo funciona </StyledHash>
+              <StyledHash to="#Technologies">Tecnologias usadas </StyledHash>
               <StyledHash to="#agents">Quiénes somos</StyledHash>
             </>
             :
