76aab01df0608f96d250c1d68a6f818f40fbeeba
EmiCon97 <derleuchtturm97@gmail.com>
2022-07-09 23:37:13 -0300

    calendar motivo

--- client/src/libs/CalendarPost/index.jsx
+++ client/src/libs/CalendarPost/index.jsx
@@ -68,17 +68,7 @@ export default function Calendar ({ operation }) {
                   </div>
                   <DivContainer contacto={true} >
                      <div className="addressWrapper"></div>               
-                     <Label className={styles.label}>Motivo</Label>
-                     <Select  className={styles.selectMotivo}                
-                        id='summary'
-                        value={summary}
-                        /* onChange={(e) => setSummary(e.target.value)} */
-                     >
-                        <option value={null}>Motivo</option>
-                        <option value={"Visita venta Mikasa Nueva"}>Vender</option>
-                        <option value={"Visita alquiler Mikasa Nueva"}>Rentar</option>
-                                                         
-                     </Select>                            
+                     <Label className={styles.label}>Motivo</Label>                         
 
                      <Label className={styles.horarioLabel}>Horario de visita: de 09 a 14hs - 30min por Visita</Label>
                      
