94bd1d86fb3da6543736d1b901469bce789a8098
lcperri <prluisca@gmail.com>
2022-07-11 15:37:25 -0500

    fix phone wsp

--- client/src/components/FormContacto/index.jsx
+++ client/src/components/FormContacto/index.jsx
@@ -33,7 +33,7 @@ export default function FormContacto() {
         dispatch(getownersphone(id));
     }, [])
 
-    let url = `https://wa.me/${phone}?text=Me%20gustaría%20saber%20el%20precio%20de%20la%20propiedad`
+    let url = `https://wa.me/${input.phoneNumber}?text=Me%20gustaría%20saber%20el%20precio%20de%20la%20propiedad`
 
     const data = {
         name: user.name + user.lastName,
