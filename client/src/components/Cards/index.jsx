e21544b21740668b05afab590cc825976a8b633e
EmiCon97 <derleuchtturm97@gmail.com>
2022-07-12 15:10:56 -0300

    cart

--- client/src/components/Cards/index.jsx
+++ client/src/components/Cards/index.jsx
@@ -63,7 +63,6 @@ const Cards = ({ id, favourites, cart }) => {
   
   return (
     <>
-<<<<<<< HEAD
       <CardsContainer>
       
         {
@@ -87,53 +86,6 @@ const Cards = ({ id, favourites, cart }) => {
         }
       </CardsContainer>
       <Pagination></Pagination>
-=======
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <>
-          <CardsContainer>
-            {id
-              ? properties &&
-                properties
-                  .slice(pages[1] - 1, pages[2])
-                  .map((e) => (
-                    <CardMisPropsPremiumVip
-                      user={user}
-                      key={e.id}
-                      idProperty={e._id}
-                      type={e.type}
-                      address={e.address}
-                      price={e.price}
-                      area={e.area}
-                      rooms={e.rooms}
-                      bathrooms={e.bathrooms}
-                      pictures={e.pictures[0]}
-                    />
-                  ))
-              : favourites
-              ? properties &&
-                properties
-                  .slice(pages[1] - 1, pages[2])
-                  .map((e) => (
-                    <Favorito key={e.id} userId={user._id} user={user} {...e} />
-                  ))
-              : cart
-              ? properties &&
-                properties
-                  .slice(pages[1] - 1, pages[2])
-                  .map((e) => <CardCart key={e.id} user={user} {...e} />)
-              : properties &&
-                properties.slice(pages[1] - 1, pages[2]).map((e) => (
-                  <StyledCard key={e._id}>
-                    <Card key={e.id} user={user} {...e} />
-                  </StyledCard>
-                ))}
-          </CardsContainer>
-          <Pagination></Pagination>
-        </>
-      )}
->>>>>>> e9e65b2ae6d4924892663624a8384d50cbea9230
     </>
   );
 };
