24e7de026eae6686aa6038c53902cc3f93c01c3d
EmiCon97 <derleuchtturm97@gmail.com>
2022-07-13 21:53:43 -0300

    success

--- client/src/components/Success/Success.jsx
+++ client/src/components/Success/Success.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
 import Swal from "sweetalert2";
 import Logo from "../../dumb/Icons/Logo";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux/es/exports";
+import { getUserById } from "../../redux/actions";
 
-export default function Success(){
+export default function Success() {
+  const navigate = useNavigate();
+  const id = localStorage.getItem("id");
+  const dispatch = useDispatch();
 
-    return(
-        <div>
-            {
-              Swal.fire({
-                 position: "top-center",
-                 icon: "success",
-                 title: "¡¡Gracias por unirte a Mikasa Nueva!!",
-                 showConfirmButton: true,
-                 /* timer: 1500, */
-                })
-            }
-        </div>
-    )
+  dispatch(getUserById(id)).then(() => {
+    Swal.fire({
+      position: "top-center",
+      icon: "success",
+      title: "Tu publicación fue creada exitosamente",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    navigate("/home");
+  });
 }
